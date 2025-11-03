0f8232a352747087821b93f3d2f65e883f2eaba6
Geovanne Washington <geoxp98@gmail.com>
2025-11-03 10:22:10 -0300

    fix some typos

--- second-largest-element/JS/secondLargest.js
+++ second-largest-element/JS/secondLargest.js
@@ -1,17 +1,14 @@
-// int arr[] = {10, -5, 20, -22, 10}; // Second largest is 10
-
 const arr = [20, -5, 20, -22, 10, 50];
 
 // Since if go through the array just once this is O(N) in time complexity
 // We could also just use any builtin sort method an return arr[arr.lengtg - 2];
 // But that would be suboptimal as the fastest sorting algorithm is O(N log N) 
 // And we don't even need to have the array sorted
-(function findSecondLargest (){
+(function findSecondLargest() {
     if (!Array.isArray(arr) || arr.length < 2 || !arr.every(e => typeof e === 'number')) {
         console.error('Invalid array');
         return;
     }
-
     let largest = -Infinity;
     let secondLargest = -Infinity;
 
