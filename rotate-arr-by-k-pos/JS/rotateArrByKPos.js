1c356c820ad66544c8e40b0cdec1f14c9e184d2f
Geovanne Washington <geoxp98@gmail.com>
2025-11-15 09:50:23 -0300

    Add solution 1 on rotating an array by k positions in JS

--- rotate-arr-by-k-pos/JS/rotateArrByKPos.js
+++ rotate-arr-by-k-pos/JS/rotateArrByKPos.js
@@ -5,37 +5,38 @@
 // --
 const arr = [1, 2, 3, 4, 5, 6, 7];
 
-function rotateArray(arr, k) {
-    // We cannot rotate an unary array
-    if (!Array.isArray(arr) || arr.length < 2 || !arr.every(e => typeof(e) === 'number')) {
-        console.error('error: invalid array');
-        return false;
-    }
-    
-    // define direction (usually when not given we asssume right)
-    let rightDirection = true;
-    let lastIndex = arr.length - 1;
-    const tempArr = new Array(k);
-    
-    if (rightDirection) {
-        for (let i = 0; i < k; i++) tempArr[i] = arr[lastIndex--];
-        
-        let pos = k;
-        for (let i = 0; i < k; i++) {
-            arr[pos++] = arr[i];
-        }
-
-        for (let i = 0; i < k; i++) {
-            arr[i] = tempArr[i];
-        }
-    }
-    
-    return true;    
+function rotate(arr, k) {
+    let arr_length = arr.length; 
+    k = k % arr_length;
+    // Why modulo operator here?
+    // Because any rotation by k positions is equivalent to rotating by the remainder when dividing 
+    // k by the array lenght. Example: rotating an array of length 7 by: 7, 14, 21... produces the 
+    // same array (full cycle)
+    // Without this reduction the algorithm would perform unnecessary work
+    if (k < 0) { k += arr.length; };
+    // Why we're checking if k is negative here?
+    // The sign of k represents the rotation in the opposite direciton.
+    // A positive k usually means 'rotate right by k'.
+    // A negative k usually means 'rotate left by |k|'
+    // Example: k = -2 -> rotate left 2 -> same as rotate right (arr.length - 2)
+    reverse(arr, 0, arr_length - 1); 
+    reverse(arr, 0, k - 1); 
+    reverse(arr, k, arr.length -1 ); 
+    return true;
 }
 
-// Where k is 3
-// Example: rotateArray(arr, 3) should change the original array to: [7, 6, 5, 1, 2, 3, 4]
-if (rotateArray(arr, 3)) console.log(arr);
+function reverse(arr, left, right) {
+    while (left < right) {
+        let tmp = arr[left];
+        arr[left] = arr[right]; 
+        arr[right] = tmp;
+        left++;
+        right--;
+    }
+}
 
-// if (rotateArray(arr, 2,)) console.log(arr);
-// Expected Output: [3, 4, 5, 6, 7, 1, 2]
+if (rotate(arr, -3)) 
+    console.log(arr);
+
+/* if (rotate(arr, 3))
+    console.log(arr); */
